fd694c770e4f96b39cd0381ced7c0d93265b8624
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-03-15 23:21:17 -0500

    commit tree 70f8871b962aec28a5af4abcba927f68d05f90b7

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const nomain = document.getElementById("main");
+nomain.remove();
+const newHeader = document.createElement("h1")
+newHeader.id = "victory";
+newHeader.innerHTML = 'Josh is the champion';
+
+
